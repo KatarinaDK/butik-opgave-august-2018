9feb44aaa06e6361704eeba30cef02bbbe380174
Katarina Petersen <8555@RTS-365.dk>
2018-08-22 15:09:22 +0200

    op

--- routes/kontakt.js
+++ routes/kontakt.js
@@ -22,77 +22,66 @@ module.exports = (app) => {
     }
   });
   app.post('/kontakt', async (req, res) => {
-    // try {
-    console.log(req.body);
+    try {
+      // console.log(req.body);
 
-    let fornavn = req.body.fornavn;
-    let efternavn = req.body.efternavn;
-    let telefon = req.body.telefon;
-    let mail = req.body.mail;
-    let emne = req.body.emne;
-    let besked = req.body.besked;
+      
 
+      // Henter felternes value og lægger ned i en variabel
+      let fornavn = req.body.fornavn;
+      let efternavn = req.body.efternavn;
+      let telefon = req.body.telefon;
+      let mail = req.body.mail;
+      let emne = req.body.emne;
+      let besked = req.body.besked;
 
-    let status = true;
+      // Sætter som default status til true
+      let status = true;
 
-    if (fornavn == "") {
+      // Tjekker de forskellige inputfelter - hvis de er tomme sættes status til false
+      if (fornavn == "") status = false;
+      if (efternavn == "") status = false;
+      if (telefon == "") status = false;
+      if (mail == "") status = false;
+      if (emne == "") status = false;
+      if (besked == "") status = false;
 
-      status = false;
+      // Hvis felterne ikke er tomme indsættes data i db og siden renderes på ny
+      if (status == true) {
+        await kontakt.createOne(fornavn, efternavn, telefon, mail, emne, besked);
+        res.render('pages/kontakt', {
+          siteTitle: 'KP',
+          pageTitle: 'Kontakt',
+          fornavn: "",
+          efternavn: "",
+          telefon: "",
+          mail: "",
+          emne: "",
+          besked: "",
+          validerMedDetSamme: 'false',
+          formBesked: "Tak for din henvendelse"
+        });
+      } else {
+        console.log("Formulardata ikke ok!");
+
+        res.render('pages/kontakt', {
+          siteTitle: 'KP',
+          pageTitle: 'Kontakt',
+          fornavn: fornavn,
+          efternavn: efternavn,
+          telefon: telefon,
+          mail: mail,
+          emne: emne,
+          besked: besked,
+          validerMedDetSamme: 'true',
+          formBesked: ''
+
+        });
+      }
     }
-    if (efternavn == "") {
-      status = false;
+    catch (e) {
+      res.send(`'Der skete en fejl: '${e.name}`);
+      console.log(e.name);
     }
-    if (telefon == "") {
-      status = false;
-    }
-    if (mail == "") {
-      status = false;
-    }
-    if (emne == "") {
-      status = false;
-    }
-    if (besked == "") {
-      status = false;
-    }
-
-
-    if (status == true) {
-      await kontakt.createOne(req.body.fornavn, req.body.efternavn, req.body.telefon, req.body.mail, req.body.emne, req.body.besked);
-      res.render('pages/kontakt', {
-        siteTitle: 'KP',
-        pageTitle: 'Kontakt',
-        fornavn: "",
-        efternavn: "",
-        telefon: "",
-        mail: "",
-        emne: "",
-        besked: "",
-        validerMedDetSamme: 'false',
-        formBesked: "Tak for din henvendelse"
-      });
-    } else {
-      console.log("Formulardata ikke ok!");
-
-      res.render('pages/kontakt', {
-        siteTitle: 'KP',
-        pageTitle: 'Kontakt',
-        fornavn: fornavn,
-        efternavn: efternavn,
-        telefon: telefon,
-        mail: mail,
-        emne: emne,
-        besked: besked,
-        validerMedDetSamme: 'true',
-        formBesked: ''
-
-      });
-    }
-
-
-    // } 
-    // catch (e) {
-    //   res.send(`'Der skete en fejl: '${e.name}`);
-    //   console.log(e.name);
-    // }
   });
 }
